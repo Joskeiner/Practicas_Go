4b287047e26aa5401703c1d8b7c354afc461187d
Joskeiner simosa Urdaneta <Joskeiner.simosa9@gmail.com>
2023-02-26 16:16:47 -0300

    manejo y parseo de json

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,32 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "COME HOME ")
+}
+
+func PostResquest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var metadata MetaData
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "error:%v", err)
+		return
+	}
+	fmt.Fprintf(w, "payload %v \n", metadata)
+}
+
+func UserPostResquest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error:%v", err)
+		return
+	}
+	response, err := user.ToJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("content-type", "application/json")
+	w.Write(response)
 }
